b9834d2171ac6b0ce546c0eb1b22e01d73e10418
Maxwell Vance <maxistheman@comcast.net>
2014-03-12 11:04:02 -0500

    user registration things

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,9 @@
 'use strict';
 
-//var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 //var Mongo = require('mongodb');
-//var User;
-//var users = global.nss.db.collection('users');
+var User;
+var users = global.nss.db.collection('users');
 //var fs = require('fs');
 //var path = require('path');
 
@@ -13,3 +13,42 @@ function User(user){
   this.password = user.password;
   this.role = user.role;
 }
+
+User.prototype.register = function(fn){
+  var self = this;
+  hashPassword(self.password, function(hashed){
+    self.password = hashed;
+    dupeCheck(self.email, function(dupeResult){
+      if (dupeResult){ //dupeCheck will return true if there is NOT a duplicate email in the DB
+        insert(self, function(err){
+          fn(err);
+        });
+      } else {
+        fn(new Error('You tried to register a duplicate user.'));
+      }
+    });
+  });
+};
+
+function insert(user, fn){
+  users.insert(user, function(err, record){
+    fn(err);
+  });
+}
+
+function hashPassword(password, fn){
+  bcrypt.hash(password, 8, function(err, hash){
+    fn(hash);
+  });
+}
+
+function dupeCheck(email, fn){
+  users.findOne({email: email}, function(foundUser){
+    if (foundUser === null){
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+}
+
--- test/unit/user_spec.js
+++ test/unit/user_spec.js
@@ -2,7 +2,9 @@
 
 process.env.DBNAME = 'airbnb-test';
 var expect = require('chai').expect;
+var Mongo = require('mongodb');
 var User;
+var sue;
 
 describe('User', function(){
   before(function(done){
@@ -15,17 +17,45 @@ describe('User', function(){
 
   beforeEach(function(done){
     global.nss.db.dropDatabase(function(err, result){
-      done();
+      sue = new User({email: 'sue@nomail.com', role:'host', password:'1234'});
+      sue.register(function(err){
+        done();
+      });
     });
   });
 
   describe('new', function(){
-    it('should create a new User object', function(){
+    it('should create a new User object', function(done){
       var u1 = new User({email: 'bob@nomail.com', role:'host', password:'1234'});
       expect(u1).to.be.instanceof(User);
       expect(u1.email).to.equal('bob@nomail.com');
       expect(u1.password).to.equal('1234');
       expect(u1.role).to.equal('host');
+      done();
     });
   });
+
+  describe('register', function(){
+    it('should register a new user', function(done){
+      var u1 = new User({email: 'bob@nomail.com', role:'host', password:'1234'});
+      u1.register(function(err){
+        expect(err).to.equal(null);
+        expect(u1.password).to.not.equal('1234');
+        expect(u1.password).to.have.length(60);
+        expect(u1._id).to.be.instanceof(Mongo.ObjectID);
+        done();
+      });
+    });
+  });
+
+  describe('dupeCheck', function(){
+    it('should not register a duplicate user', function(done){
+      var u1 = new User({email: 'sue@nomail.com', role:'host', password:'1234'});
+      u1.register(function(err){
+        expect(typeof err).to.equal('object'); //Errors are of type object
+        done();
+      });
+    });
+  });
+
 });
